79af0c466a7da8e207ef45aae9e56995fabb59eb
Patrick McCarthy <resume@patrickmccarthy.cc>
2020-09-09 15:51:53 -0400

    add kfp code, update credentials

---
+++ conf_values.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set +x
+CLUSTER_NAME=cluster-5
+GOOGLE_CLOUD_PROJECT=dst-mlpipes
+ZONE=us-central1-a
+set -x
---
+++ credentials.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set +x
+
+source conf_values.sh
+
+gcloud container clusters get-credentials $CLUSTER_NAME --zone=us-central1-a
+
+pushd $HOME
+
+curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 
+chmod +x skaffold
+
+pip install -U pip --no-input
+pip install tfx kfp --use-feature=2020-resolver --no-input
+
+export GOOGLE_CLOUD_PROJECT=$GOOGLE_CLOUD_PROJECT
+
+popd;
+
+set -x
