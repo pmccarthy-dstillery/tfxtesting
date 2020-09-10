9fc8b11531949d434638b44736b5f378af01b877
Patrick McCarthy <pmccarthy@MacBook-Air.local>
2020-09-10 11:54:19 -0400

    reinstate steps in fit

--- kfp/models/keras/model.py
+++ kfp/models/keras/model.py
@@ -146,6 +146,9 @@ def run_fn(fn_args):
 
   model.fit(
       train_dataset,
+      steps_per_epoch=fn_args.train_steps,
+      validation_data=eval_dataset,
+      validation_steps=fn_args.eval_steps,
       validation_data=eval_dataset,
       callbacks=[tensorboard_callback])
 
